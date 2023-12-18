ea7cbfbaa7a234689ec3de0a3d931eeed01f5809
zmohammadtabar <zmohammadtabar@payeshgaran.net>
2023-12-18 11:29:39 +0330

    - c3

--- src/main/java/com/rahand/idea/channel/dto/deposit/ActionType.java
+++ src/main/java/com/rahand/idea/channel/dto/deposit/ActionType.java
@@ -2,10 +2,6 @@ package com.rahand.idea.channel.dto.deposit;
 
 import io.swagger.annotations.ApiModel;
 
-/**
- * @author aglitchyy
- * @created 02/11/2021
- */
 @ApiModel
 public enum ActionType {
     CREDIT, DEBIT;
